5df34fdf5bdc18399b7b7c6928fe212e841af719
Lars Espen Baanerud <126690594+larsespenssb@users.noreply.github.com>
2024-02-01 10:58:17 +0100

    wip: config examples

---
+++ config_examples/ConfigReader.py
@@ -0,0 +1,21 @@
+from pathlib import Path
+import tomli
+import dapla as dp
+
+class ConfigReader:
+    def __init__(self, config_file=None):
+        if not config_file:
+            config_file=dp.repo_root_dir() / "config" / "config.toml"
+        
+        with open(config_file, mode="rb") as f:
+            self.config = tomli.load(f)
+           
+    #petter solberg  was here!!
+    def klargjort_dir(self):
+        return f"{self.config['storage']['bucket1'] + self.config['paths']['klargjort_dir']}"
+        
+        
+        
+
+        
+        
