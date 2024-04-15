173b52c1879bf546874d2933ef41346bacce48b9
Arne Sørli <81353974+arneso-ssb@users.noreply.github.com>
2024-04-15 11:09:16 +0200

    Add file file_encryption.py

---
+++ secret/file_encryption.py
@@ -0,0 +1,17 @@
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#   kernelspec:
+#     display_name: tech-coach-examples
+#     language: python
+#     name: tech-coach-examples
+# ---
+
+# %% [markdown]
+# # Kryptering av filer
+
+# %%
