047ae25fbe962b5ca60a5057181d6030531b086b
Arne Sørli <81353974+arneso-ssb@users.noreply.github.com>
2024-08-30 20:31:52 +0200

    TIP-844: Test local logging to gcp

--- src/logging/gcp_logger.py
+++ src/logging/gcp_logger.py
@@ -5,10 +5,14 @@ from dapla import AuthClient
 
 
 credentials = AuthClient.fetch_google_credentials()
+client = google.cloud.logging.Client(
+    project="dev-tech-coach-6cc5", credentials=credentials
+)
 
-# Cloud Logging handler
-client = google.cloud.logging.Client(project="tech-coach-p-ku", credentials=credentials)
-# cloud_handler = CloudLoggingHandler(client)
+# This works in a google cloud shell
+# client = google.cloud.logging.Client()
 client.setup_logging()
+logging.warning("Test of python logging from local PC, Arne")
 
-logging.warning("This is a test log messagage from python!")
+logger = logging.getLogger(__name__)
+logger.warning("Test of python logging from local PC, standard python logging, Arne")
