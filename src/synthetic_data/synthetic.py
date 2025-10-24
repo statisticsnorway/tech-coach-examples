931c1122326c7439a72fc0a9bdb81134ae78eb05
Arne Sørli <81353974+arneso-ssb@users.noreply.github.com>
2025-10-24 09:40:22 +0200

    Example using of standard faker

--- src/synthetic_data/synthetic.py
+++ src/synthetic_data/synthetic.py
@@ -12,11 +12,38 @@
 # ---
 
 # %%
-from faker import Faker
 import pandas as pd
+from faker import Faker
+
+
+# %% [markdown]
+# # Enkelt bruk av standard faker
+# Dette viser enkel bruk av standard faker til å generere norske navn, adresser osv.
 
 # %%
 fake = Faker("no_NO")
 print(fake.name())
 print(fake.address())
 print(fake.phone_number())
+print(fake.license_plate())
+
+
+# %%
+def generate_faker_df(rows: int) -> pd.DataFrame:
+    fake = Faker("no_NO")
+    data = []
+    for _ in range(rows):
+        data.append(
+            {
+                "name": fake.name(),
+                "address": fake.street_address(),
+                "phone": fake.phone_number(),
+                "license-plate": fake.license_plate(),
+            }
+        )
+    return pd.DataFrame(data)
+
+
+# %%
+df = generate_faker_df(10)
+df.head()
