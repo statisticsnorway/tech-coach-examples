cc25709b23347ca57a5ad6800055341215e5194e
ssb-dor <dor@ssb.no>
2026-07-01 08:58:46 +0200

    Testing

--- src/sonarcube_fixes/test.py
+++ src/sonarcube_fixes/test.py
@@ -7,3 +7,7 @@ db.sql(
 db.sql(
     f"""select distinct {navn}, verdi as belop_50 from r12_koblet2 where art in ('01','60')"""
 ).create_view("splitt_0160")
+
+sp1=db.sql(f"""select a.*, b.belop_50 from splitt_7172 a full join splitt_0160 b 
+on a.periode=b.periode and a.selskap=b.selskap and a.serienavn=b.serienavn and a.ltid=b.ltid and a.sek=b.sek
+where a.rente is not null and b.belop_50 is not null""").df()
