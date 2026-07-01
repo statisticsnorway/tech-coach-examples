3ed1054b55f7109c13191d149cf87e7ec4ea1c1b
ssb-dor <dor@ssb.no>
2026-07-01 08:39:20 +0200

    Testing fix, now make fix

--- src/sonarcube_fixes/test.py
+++ src/sonarcube_fixes/test.py
@@ -1,9 +1,9 @@
 import duckdb as db
 
 navn = "eksempel"
-splitt_7172 = db.sql(
+db.sql(
     f"""select distinct {navn}, verdi as rente from r12_koblet2 where art in ('71','72')"""
-)
-splitt_0160 = db.sql(
+).create_view("splitt_7172")
+db.sql(
     f"""select distinct {navn}, verdi as belop_50 from r12_koblet2 where art in ('01','60')"""
-)
+).create_view("splitt_0160")
